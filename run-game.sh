7859111dd0d78d69ae0f979a51608f0a2d38d95d
Igor <Igor_Petrov@epam.com>
2019-01-23 13:49:26 +0400

    add game running script

--- run-game.sh
+++ run-game.sh
@@ -1,2 +1,26 @@
 #/usr/bin/env bash
-java -jar -Dserver.port=8081 build/libs/hardinv-0.0.1-SNAPSHOT.jar --spring.datasource.host=epruizhsa0001t2 --spring.datasource.port=5433
+
+REBUILD=false
+PORT=8081
+DB_PORT=5433
+DB_HOST=epruizhsa0001t2
+
+while true; do
+	case "$1" in
+		-b | --build) REBUILD=true; shift ;;
+		-p | --port) PORT="$2"; shift ;;
+		-d | --db-port) DB_PORT="$2"; shift ;;
+		-h | --db-host) DB_HOST="$s"; shift ;;
+		* ) break ;;
+	esac
+done
+
+if [ "$REBUILD" = true ]; then
+	./gradlew clean build
+	if [ $? -ne 0 ]; then
+		echo "Build failed"
+		exit 1
+	fi
+fi
+
+java -jar -Dserver.port="$PORT" build/libs/hardinv-0.0.1-SNAPSHOT.jar --spring.datasource.host="$DB_HOST" --spring.datasource.port="$DB_PORT"
