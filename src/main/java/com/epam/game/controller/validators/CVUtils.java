a9b971ce1c8a0c3fd016218158777bade6f7c61c
Igor Petrov <Igor_Petrov@epam.com>
2019-01-24 11:02:15 +0400

    allow any valid user email

--- src/main/java/com/epam/game/controller/validators/CVUtils.java
+++ src/main/java/com/epam/game/controller/validators/CVUtils.java
@@ -15,12 +15,4 @@ public class CVUtils {
     public static boolean inRange(int num, int min, int max) {
         return num >= min && num <= max;
     }
-
-    public static boolean isCorrectPhoneNumber(String phone) {
-        return phone.matches("[\\+]{0,1}(\\(?[0-9]\\)?[\\s-]?){5,12}");
-    }
-
-    public static boolean isGoodEmailPart(String email) {
-        return email.matches("[\\w\\._\\d]+") && inRange(email.length(), 4,200);
-    }
 }
