d2772d4304ca77852d15376a410d699cf8a6fd3d
Igor Petrov <Igor_Petrov@epam.com>
2019-01-25 15:24:41 +0400

    fix MVC conf

--- src/main/java/com/epam/game/HardinvApplication.java
+++ src/main/java/com/epam/game/HardinvApplication.java
@@ -2,9 +2,16 @@ package com.epam.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HardinvApplication {
+public class HardinvApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HardinvApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HardinvApplication.class, args);
